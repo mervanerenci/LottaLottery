a2d8ba27c4b0f54ba793bde4f4a8e460347f6e5a
Mervan <mervanerenci@gmail.com>
2024-01-14 18:45:09 +0300

    Add Round struct and constants

--- contracts/Powerball.sol
+++ contracts/Powerball.sol
@@ -3,7 +3,20 @@
 pragma solidity ^0.8.0;
 
 contract Powerball {
-        
+        struct Round {
+            uint endTime;
+            uint drawBlock;
+            uint[6] winningNumbers;
+            mapping(address => uint[6][]) tickets;
+        }
+
+        uint public constant TICKET_PRICE = 2e15;
+        uint public constant MAX_NUMBER = 69;
+        uint public constant MAX_POWERBALL_NUMBER = 26;
+        uint public constant ROUND_LENGTH = 3 days;
+
+        uint public round;
+        mapping(uint => Round) public rounds;
 
 
 }
