b5af78c50d35b24e1328b30ae706b1571d4f0379
Mervan <mervanerenci@gmail.com>
2024-01-14 18:54:13 +0300

    Add drawNumbers and claim functions for Powerball game

--- contracts/Powerball.sol
+++ contracts/Powerball.sol
@@ -46,8 +46,58 @@ contract Powerball {
             rounds[round].endTime = now + ROUND_LENGTH;
         }
 
-        for (i=0; i < numbers.length; i++) {
+        for (i = 0; i < numbers.length; i++) {
             rounds[round].tickets[msg.sender].push(numbers[i]);
         }
+    }
+
+    function drawNumbers(uint _round) public {
+        uint drawBlock = rounds[_round].drawBlock;
+        require(now > rounds[_round].endTime);
+        require(block.number >= drawBlock);
+        require(rounds[_round].winningNumbers[0] == 0);
+        for (uint i = 0; i < 5; i++) {
+            bytes32 rand = keccak256(block.blockhash(drawBlock), i);
+            uint numberDraw = (uint(rand) % MAX_NUMBER) + 1;
+            rounds[_round].winningNumbers[i] = numberDraw;
+        }
+        rand = keccak256(block.blockhash(drawBlock), uint(5));
+        uint powerballDraw = (uint(rand) % MAX_POWERBALL_NUMBER) + 1;
+        rounds[_round].winningNumbers[5] = powerballDraw;
+    }
+
+    function claim(uint _round) public {
+        require(rounds[_round].tickets[msg.sender].length > 0);
+        require(rounds[_round].winningNumbers[0] != 0);
+        uint[6][] storage myNumbers = rounds[_round].tickets[msg.sender];
+        uint[6] storage winningNumbers = rounds[_round].winningNumbers;
+        uint payout = 0;
+        for (uint i = 0; i < myNumbers.length; i++) {
+            uint numberMatches = 0;
+            for (uint j = 0; j < 5; j++) {
+                for (uint k = 0; k < 5; k++) {
+                    if (myNumbers[i][j] == winningNumbers[k])
+                        numberMatches += 1;
+                }
+            }
+            bool powerballMatches = (myNumbers[i][5] == winningNumbers[5]);
+            // win conditions
+            if (numberMatches == 5 && powerballMatches) {
+                payout = this.balance;
+                break;
+            } else if (numberMatches == 5) payout += 1000 ether;
+            else if (numberMatches == 4 && powerballMatches) payout += 50 ether;
+            else if (numberMatches == 4)
+                payout += 1e17; // .1 ether
+            else if (numberMatches == 3 && powerballMatches)
+                payout += 1e17; // .1 ether
+            else if (numberMatches == 3)
+                payout += 7e15; // .007 ether
+            else if (numberMatches == 2 && powerballMatches)
+                payout += 7e15; // .007 ether
+            else if (powerballMatches) payout += 4e15; // .004 ether
+        }
+        msg.sender.transfer(payout);
+        delete rounds[_round].tickets[msg.sender];
     }
 }
